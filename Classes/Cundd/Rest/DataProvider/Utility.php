72c1a4c9320f5be3da899855ba6674804d5c3b3e
Daniel Corn <d@cundd.net>
2013-06-19 12:18:04 +0200

    Added feature to automatically try to convert plural resource names to singular

--- Classes/Cundd/Rest/DataProvider/Utility.php
+++ Classes/Cundd/Rest/DataProvider/Utility.php
@@ -4,6 +4,7 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * A utility class with static methods for Data Providers
+ *
  * @package Cundd\Rest\DataProvider
  */
 class Utility {
@@ -17,10 +18,12 @@ class Utility {
 	 *     MyExt
 	 *     MyModel
 	 *   )
-	 * @param $path
+	 *
+	 * @param string $path
+	 * @param bool $convertPlural Indicates if plural resource names should be converted
 	 * @return array
 	 */
-	static public function getClassNamePartsForPath($path) {
+	static public function getClassNamePartsForPath($path, $convertPlural = TRUE) {
 		if (strpos($path, '_') !== FALSE) {
 			$path = GeneralUtility::underscoredToUpperCamelCase($path);
 		}
@@ -28,7 +31,61 @@ class Utility {
 		if (count($parts) < 3) {
 			array_unshift($parts, '');
 		}
-		$parts = array_map(function($part) {return ucfirst($part);}, $parts);
+
+		if ($convertPlural && $parts) {
+			$lastPartIndex = count($parts) - 1;
+			$parts[$lastPartIndex] = static::singularize($parts[$lastPartIndex]);
+		}
+
+		$parts = array_map(function ($part) {
+			return ucfirst($part);
+		}, $parts);
 		return $parts;
+	}
+
+	/**
+	 * Tries to convert an english plural into it's singular.
+	 *
+	 * @param string $word
+	 * @return string
+	 */
+	static public function singularize($word) {
+		// Here is the list of rules. To add a scenario,
+		// Add the plural ending as the key and the singular
+		// ending as the value for that key. This could be
+		// turned into a preg_replace and probably will be
+		// eventually, but for now, this is what it is.
+		//
+		// Note: The first rule has a value of false since
+		// we don't want to mess with words that end with
+		// double 's'. We normally wouldn't have to create
+		// rules for words we don't want to mess with, but
+		// the last rule (s) would catch double (ss) words
+		// if we didn't stop before it got to that rule.
+		$rules = array(
+			'ss' => false,
+			'os' => 'o',
+			'ies' => 'y',
+			'xes' => 'x',
+			'oes' => 'o',
+			'ies' => 'y',
+			'ves' => 'f',
+			's' => '');
+		// Loop through all the rules and do the replacement.
+		foreach (array_keys($rules) as $key) {
+			// If the end of the word doesn't match the key,
+			// it's not a candidate for replacement. Move on
+			// to the next plural ending.
+			if (substr($word, (strlen($key) * -1)) != $key)
+				continue;
+			// If the value of the key is false, stop looping
+			// and return the original version of the word.
+			if ($key === false)
+				return $word;
+			// We've made it this far, so we can do the
+			// replacement.
+			return substr($word, 0, strlen($word) - strlen($key)) . $rules[$key];
+		}
+		return $word;
 	}
 }
