c74278080593c099a701eb4ddab5001817bd76d6
Jens Jacobsen <jens.jacobsen@ueberbit.de>
2015-10-01 11:43:33 +0200

    [TASK] Update the autoloader call

--- Tests/Bootstrap.php
+++ Tests/Bootstrap.php
@@ -6,5 +6,5 @@
 if (file_exists(__DIR__ . '/../vendor/')) {
     require_once __DIR__ . '/../vendor/autoload.php';
 } else {
-    Tx_CunddComposer_Autoloader::register();
+    \Cundd\CunddComposer\Autoloader::register();
 }
