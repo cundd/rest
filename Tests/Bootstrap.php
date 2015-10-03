21b871f725b7f025cdf3607dccb4e919f6744a57
Daniel Corn <d@cundd.net>
2015-10-03 14:22:42 +0200

    Skip bootstrapSystem of \TYPO3\CMS\Core\Build\FunctionalTestsBootstrap() if TYPO3_MODE is already defined

--- Tests/Bootstrap.php
+++ Tests/Bootstrap.php
@@ -14,6 +14,9 @@ class Bootstrap {
     }
 
     protected function setupTYPO3() {
+        if (defined('TYPO3_MODE')) {
+            return;
+        }
         $restTypo3BasePath = getenv('REST_TYPO3_BASE_PATH');
         if ($restTypo3BasePath) {
             if (file_exists($restTypo3BasePath . '/typo3/' . self::TYPO3_BOOTSTRAP_CLASS_PATH)) {
