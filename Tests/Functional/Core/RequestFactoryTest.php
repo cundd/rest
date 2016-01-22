4bd1df37fecb09933ab0fefddc0668e0f675b784
Daniel Corn <d@cundd.net>
2016-01-22 13:44:10 +0100

    Added test if the Request factory strips the query from path und url

--- Tests/Functional/Core/RequestFactoryTest.php
+++ Tests/Functional/Core/RequestFactoryTest.php
@@ -358,6 +358,28 @@ class RequestFactoryTest extends AbstractCase {
     }
 
     /**
+     * @test
+     */
+    public function urlAndPathShouldNotIncludeQueryDataTest() {
+        $_GET['u'] = 'MyExt-MyModel/1?query=string';
+        $request = $this->fixture->getRequest();
+        $this->assertEquals('MyExt-MyModel', $request->path());
+        $this->assertEquals('MyExt-MyModel/1', $request->url());
+        $this->assertEquals('json', $request->format());
+    }
+
+    /**
+     * @test
+     */
+    public function urlAndPathShouldNotIncludeQueryDataFromRequestUriTest() {
+        $_SERVER['REQUEST_URI'] = '/rest/MyExt-MyModel/1?query=string';
+        $request = $this->fixture->getRequest();
+        $this->assertEquals('MyExt-MyModel', $request->path());
+        $this->assertEquals('MyExt-MyModel/1', $request->url());
+        $this->assertEquals('json', $request->format());
+    }
+
+    /**
      * @param array ...$configurationProviderSetting
      * @return RequestFactory
      */
