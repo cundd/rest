1653ae21d6baf3b4c17f237e02e7c4c2b4ebd01f
Daniel Corn <info@cundd.net>
2017-02-04 10:30:10 +0100

    Added initial support for TYPO3 8 testing_framework

--- Tests/Functional/Bootstrap.php
+++ Tests/Functional/Bootstrap.php
@@ -21,22 +21,22 @@ class Bootstrap
         if (defined('TYPO3_MODE') && defined('ORIGINAL_ROOT')) {
             return;
         }
-        $restTypo3BasePath = getenv('REST_TYPO3_BASE_PATH');
-        if ($restTypo3BasePath === false) {
-            $restTypo3BasePath = getenv('TYPO3_PATH_WEB');
-        }
+        $restTypo3BasePath = $this->detectTypo3BasePath();
 
-        if ($restTypo3BasePath) {
+        if ($restTypo3BasePath !== false) {
+            $typo3Version8LtsPath = $restTypo3BasePath . '/components/testing_framework/Resources/Core/Build/FunctionalTestsBootstrap.php';
+
             if (file_exists($restTypo3BasePath . '/typo3/' . self::TYPO3_BOOTSTRAP_CLASS_PATH)) {
                 require_once $restTypo3BasePath . '/typo3/' . self::TYPO3_BOOTSTRAP_CLASS_PATH;
+
+                return;
+            } elseif (file_exists($typo3Version8LtsPath)) {
+                require_once $typo3Version8LtsPath;
 
                 return;
             }
         }
 
-        if (!$this->lookForTypo3(realpath(__DIR__) ?: __DIR__)) {
-            $this->lookForTypo3(getcwd());
-        }
         if (!class_exists('TYPO3\CMS\Core\Build\FunctionalTestsBootstrap')) {
             throw new \Exception('TYPO3\CMS\Core\Build\FunctionalTestsBootstrap not found');
         }
@@ -63,25 +63,66 @@ class Bootstrap
         require_once __DIR__ . '/AbstractCase.php';
     }
 
-    private function lookForTypo3($startPath)
+    private function getTYPO3InstallationPath($startPath)
     {
         $cur = $startPath;
         while ($cur !== '/') {
             if (file_exists($cur . '/typo3/' . self::TYPO3_BOOTSTRAP_CLASS_PATH)) {
-                require_once $cur . '/typo3/' . self::TYPO3_BOOTSTRAP_CLASS_PATH;
-
-                return true;
+                return $cur . '/typo3/' . self::TYPO3_BOOTSTRAP_CLASS_PATH;
             } elseif (file_exists($cur . '/TYPO3.CMS/typo3/' . self::TYPO3_BOOTSTRAP_CLASS_PATH)) {
-                require_once $cur . '/TYPO3.CMS/typo3/' . self::TYPO3_BOOTSTRAP_CLASS_PATH;
-
-                return true;
+                return $cur . '/TYPO3.CMS/typo3/' . self::TYPO3_BOOTSTRAP_CLASS_PATH;
             }
 
             $cur = dirname($cur);
-
         }
 
+        return '';
+    }
+
+    /**
+     * @param string $environmentKey
+     * @return array|bool|false|string
+     */
+    protected function checkEnvironmentForTypo3BasePath($environmentKey)
+    {
+        $restTypo3BasePath = getenv((string)$environmentKey);
+        if ($restTypo3BasePath === false) {
+            return false;
+        }
+
+        if (file_exists($restTypo3BasePath)) {
+            return $restTypo3BasePath;
+        }
+
+        $this->printWarning('TYPO3 installation in %s "%s" not found', $environmentKey, $restTypo3BasePath);
+
         return false;
+    }
+
+    /**
+     * @return array|bool|false|string
+     */
+    protected function detectTypo3BasePath()
+    {
+        $restTypo3BasePath = $this->checkEnvironmentForTypo3BasePath('REST_TYPO3_BASE_PATH');
+        if ($restTypo3BasePath === false) {
+            $restTypo3BasePath = $this->checkEnvironmentForTypo3BasePath('TYPO3_PATH_WEB');
+        }
+        if ($restTypo3BasePath === false) {
+            $restTypo3BasePath = $this->getTYPO3InstallationPath(realpath(__DIR__) ?: __DIR__);
+        }
+        if ($restTypo3BasePath === false) {
+            $restTypo3BasePath = $this->getTYPO3InstallationPath(realpath(getcwd()) ?: getcwd());
+
+            return $restTypo3BasePath;
+        }
+
+        return $restTypo3BasePath;
+    }
+
+    private function printWarning($message, ...$arguments)
+    {
+        fwrite(STDERR, vsprintf((string)$message, $arguments) . PHP_EOL);
     }
 }
 
