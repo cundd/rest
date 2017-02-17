13f10a4c3daa1c6a806efc463835c4dc272299a4
Daniel Corn <info@cundd.net>
2017-02-17 18:35:14 +0100

    Removed Document Storage related entries
    Removed TCA
    Removed database

--- ext_tables.php
+++ ext_tables.php
@@ -3,8 +3,5 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_rest_domain_model_document', 'EXT:rest/Resources/Private/Language/locallang_csh_tx_rest_domain_model_document.xlf');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_rest_domain_model_document');
-
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/Page', 'Virtual Object - Page');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript/Content', 'Virtual Object - Content');
