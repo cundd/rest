a31860d36aea253e149c4c7d5dda22eb6b2685b8
Daniel Corn <cod@iresults.li>
2018-10-25 14:31:36 +0200

    Fix test autoloading when installed as a library

--- Tests/AbstractBootstrap.php
+++ Tests/AbstractBootstrap.php
@@ -20,6 +20,8 @@ abstract class AbstractBootstrap
         // Load composer autoloader
         if (file_exists(__DIR__ . '/../vendor/')) {
             require_once __DIR__ . '/../vendor/autoload.php';
+        } elseif (file_exists(__DIR__ . '/../../../autoload.php')) {
+            require_once __DIR__ . '/../../../autoload.php';
         } else {
             if (!class_exists('Cundd\\CunddComposer\\Autoloader')) {
                 require_once __DIR__ . '/../../../cundd_composer/Classes/Autoloader.php';
