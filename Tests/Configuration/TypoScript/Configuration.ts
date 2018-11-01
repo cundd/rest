0cf58a589936ad9ca2c4cd24eeca0887614bf154
Daniel Corn <info@cundd.net>
2018-11-01 23:05:41 +0100

    Add Virtual Objects Page configuration for TYPO3 9 LTS

--- Tests/Configuration/TypoScript/Configuration.ts
+++ Tests/Configuration/TypoScript/Configuration.ts
@@ -1,8 +1,13 @@
-### Include this file to configure TYPO3 for the manual tests
+### Include this file to configure TYPO3 for the manual tests and the correct Virtual Object Page setup
 # <INCLUDE_TYPOSCRIPT: source="FILE:EXT:rest/Tests/Configuration/TypoScript/Configuration.ts">
+#
+# Virtual Object Page setup for TYPO3 < 9
+# <INCLUDE_TYPOSCRIPT: source="FILE:EXT:rest/Configuration/TypoScript/Page/TYPO3-8/setup.txt">
+
+# Virtual Object Page setup for TYPO3 >= 9
+# <INCLUDE_TYPOSCRIPT: source="FILE:EXT:rest/Configuration/TypoScript/Page/TYPO3-9/setup.txt">
 
 <INCLUDE_TYPOSCRIPT: source="FILE:EXT:rest/Configuration/TypoScript/Content/setup.txt">
-<INCLUDE_TYPOSCRIPT: source="FILE:EXT:rest/Configuration/TypoScript/Page/setup.txt">
 
 plugin.tx_rest.settings {
     paths {
--- ext_tables.php
+++ ext_tables.php
@@ -4,25 +4,33 @@ if (!defined('TYPO3_MODE')) {
 }
 
 call_user_func(
-    function ($extKey) {
+    function () {
+        if (version_compare(TYPO3_version, '9.5.0') >= 0) {
+            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+                'rest',
+                'Configuration/TypoScript/Page/TYPO3-9',
+                'Virtual Object - Page'
+            );
+        } else {
+            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+                'rest',
+                'Configuration/TypoScript/Page/TYPO3-8',
+                'Virtual Object - Page'
+            );
+        }
+
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-            $extKey,
-            'Configuration/TypoScript/Page',
-            'Virtual Object - Page'
-        );
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-            $extKey,
+            'rest',
             'Configuration/TypoScript/Content',
             'Virtual Object - Content'
         );
 
 
-        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports'][$extKey]['handler'] = [
-            'title' => 'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_db.xlf:reports.handler.title',
-            'description' => 'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_db.xlf:reports.handler.description',
-//            'icon' => 'EXT:' . $extKey . '/Resources/Public/Icons/tx_additionalreports_' . $report[1] . '.png',
-            'report' => \Cundd\Rest\Documentation\HandlerReport::class,
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['rest']['handler'] = [
+            'title'       => 'LLL:EXT:rest/Resources/Private/Language/locallang_db.xlf:reports.handler.title',
+            'description' => 'LLL:EXT:rest/Resources/Private/Language/locallang_db.xlf:reports.handler.description',
+//            'icon' => 'EXT:rest/Resources/Public/Icons/tx_additionalreports_' . $report[1] . '.png',
+            'report'      => \Cundd\Rest\Documentation\HandlerReport::class,
         ];
-    },
-    $_EXTKEY
+    }
 );
