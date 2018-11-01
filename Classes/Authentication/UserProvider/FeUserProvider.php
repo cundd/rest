40f30e1b9967945e1d5190abcc753226b678b84a
Daniel Corn <info@cundd.net>
2018-11-01 23:05:17 +0100

    Use the Virtual Objects backend to test for FE Users

--- Classes/Authentication/UserProvider/FeUserProvider.php
+++ Classes/Authentication/UserProvider/FeUserProvider.php
@@ -3,6 +3,8 @@
 namespace Cundd\Rest\Authentication\UserProvider;
 
 use Cundd\Rest\Authentication\UserProviderInterface;
+use Cundd\Rest\VirtualObject\Persistence\Backend;
+use Cundd\Rest\VirtualObject\Persistence\QueryInterface;
 
 /**
  * User Provider implementation for FeUsers
@@ -23,67 +25,33 @@ class FeUserProvider implements UserProviderInterface
      */
     public function checkCredentials($username, $password)
     {
-        $databaseAdapter = $this->getDatabaseAdapter();
-
-        $whereClause = $this->buildWhereStatement(
-            [
-                'username' => $username,
-                'password' => $password,
-            ]
-        );
-        $result = $databaseAdapter->exec_SELECTquery('COUNT(*)', 'fe_users', $whereClause);
-        $row = $databaseAdapter->sql_fetch_row($result);
-
-        return (bool)$row[0];
-    }
-
-    /**
-     * Builds the where statement from the given properties
-     *
-     * @param array $properties
-     * @return string
-     */
-    protected function buildWhereStatement($properties)
-    {
-        $whereParts = [];
-        $databaseAdapter = $this->getDatabaseAdapter();
-        foreach ($properties as $key => $value) {
-            if ($key === 'password') {
-                list($value, $key) = $this->preparePassword($value);
-            }
-            if (is_int($value)) {
-                $whereParts[] = '`' . $key . '`=' . $value;
-            } else {
-                $whereParts[] = '`' . $key . '`=' . $databaseAdapter->fullQuoteStr($value, 'fe_users');
-            }
+        if (!is_string($password) || '' === $password) {
+            return false;
         }
-        $whereParts[] = '`' . self::PASSWORD_COLUMN_NAME . '`<>\'\'';
-        $whereParts[] = '(`disable`=0)';
-        $whereParts[] = '(`deleted`=0)';
-        $whereParts[] = sprintf('(`starttime`<=%d)', time());
-        $whereParts[] = sprintf('(endtime=0 OR endtime>%d)', time());
 
-        return implode(' AND ', $whereParts);
-    }
+        $backend = new Backend();
+        $query = [
+            'username'                 => $username,
+            self::PASSWORD_COLUMN_NAME => $password,
+            'disable'                  => 0,
+            'deleted'                  => 0,
+            'starttime'                => [
+                'value'    => time(),
+                'operator' => QueryInterface::OPERATOR_LESS_THAN_OR_EQUAL_TO,
+            ],
+        ];
 
-    /**
-     * Returns an array containing the prepared password and table column
-     *
-     * @param string $password
-     * @return array
-     */
-    protected function preparePassword($password)
-    {
-        return [$password, self::PASSWORD_COLUMN_NAME];
-    }
+        $endtimeZero = [
+            'endtime' => 0,
+        ];
+        $endtimeGtNow = [
+            'endtime' => [
+                'value'    => time(),
+                'operator' => QueryInterface::OPERATOR_GREATER_THAN,
+            ],
+        ];
 
-    /**
-     * Returns the database adapter
-     *
-     * @return \TYPO3\CMS\Core\Database\DatabaseConnection
-     */
-    protected function getDatabaseAdapter()
-    {
-        return $GLOBALS['TYPO3_DB'];
+        return 0 < $backend->getObjectCountByQuery('fe_users', array_merge($query, $endtimeZero))
+            || 0 < $backend->getObjectCountByQuery('fe_users', array_merge($query, $endtimeGtNow));
     }
 }
