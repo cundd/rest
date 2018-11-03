09c614a4393934c6bb7f7809fc42e31c43d50806
Daniel Corn <info@cundd.net>
2018-11-03 18:23:33 +0100

    Workaround for resolving the Configuration Provider implementation in TYPO3 9

---
+++ Classes/Configuration/ConfigurationProvider.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Cundd\Rest\Configuration;
+
+/**
+ * Workaround to enable loading the correct implementation for `ConfigurationProviderInterface`
+ */
+class ConfigurationProvider extends TypoScriptConfigurationProvider
+{
+}
