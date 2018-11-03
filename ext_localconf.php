f3751685bb4aeb2c83f384c27639f6b59f3f1914
Daniel Corn <info@cundd.net>
2018-11-03 18:24:24 +0100

    Refactor localconf and add XCLASS definitions

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,26 +3,42 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['rest'] = \Cundd\Rest\BootstrapDispatcher::class . '::processRequest';
+call_user_func(
+    function () {
+        // Register eID
+        $GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['rest'] = \Cundd\Rest\BootstrapDispatcher::class . '::processRequest';
 
-call_user_func(function () {
-    if (isset($_SERVER['REQUEST_URI'])) {
-        $restRequestBasePath = (string)getenv('TYPO3_REST_REQUEST_BASE_PATH');
+        // Detect and "hijack" REST requests
+        if (isset($_SERVER['REQUEST_URI'])) {
+            $restRequestBasePath = (string)getenv('TYPO3_REST_REQUEST_BASE_PATH');
 
-        if ($restRequestBasePath) {
-            $restRequestBasePath = '/' . trim($restRequestBasePath, '/');
+            if ($restRequestBasePath) {
+                $restRequestBasePath = '/' . trim($restRequestBasePath, '/');
+            }
+
+            $restRequestPrefix = $restRequestBasePath . '/rest/';
+            $restRequestPrefixLength = strlen($restRequestPrefix);
+            $requestUri = $_SERVER['REQUEST_URI'];
+
+            if (substr($requestUri, 0, $restRequestPrefixLength) === $restRequestPrefix) {
+                $_GET['eID'] = 'rest';
+            }
         }
 
-        $restRequestPrefix = $restRequestBasePath . '/rest/';
-        $restRequestPrefixLength = strlen($restRequestPrefix);
-        $requestUri = $_SERVER['REQUEST_URI'];
-
-        if (substr($requestUri, 0, $restRequestPrefixLength) === $restRequestPrefix) {
-            $_GET['eID'] = 'rest';
+        // Register Cache
+        if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cundd_rest_cache'])) {
+            $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cundd_rest_cache'] = [];
         }
+
+        // Register implementation classes
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\Cundd\Rest\Configuration\ConfigurationProviderInterface::class] = [
+            'className' => \Cundd\Rest\Configuration\TypoScriptConfigurationProvider::class,
+        ];
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][\Cundd\Rest\Authentication\UserProviderInterface::class] = [
+            'className' => \Cundd\Rest\Authentication\UserProvider\FeUserProvider::class,
+        ];
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][Cundd\Rest\Handler\HandlerInterface::class] = [
+            'className' => \Cundd\Rest\Handler\CrudHandler::class,
+        ];
     }
-});
-
-if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cundd_rest_cache'])) {
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cundd_rest_cache'] = array();
-}
+);
