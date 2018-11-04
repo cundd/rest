c5a8823cbb5a77bb0ac8b8cf7fc5c9cf8ae4f6e2
Daniel Corn <info@cundd.net>
2018-11-04 11:55:35 +0100

    Code formatting

--- Configuration/TCA/Overrides/fe_users.php
+++ Configuration/TCA/Overrides/fe_users.php
@@ -4,17 +4,25 @@ if (!defined('TYPO3_MODE')) {
     die ('Access denied.');
 }
 
-$tempColumns = array(
-    'tx_rest_apikey' => array(
+$tempColumns = [
+    'tx_rest_apikey' => [
         'exclude' => 1,
-        'label' => 'API Key',
-        'config' => array(
+        'label'   => 'API Key',
+        'config'  => [
             'type' => 'input',
             'size' => '30',
             'eval' => 'nospace',
-        )
-    ),
-);
+        ],
+    ],
+];
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('fe_users', $tempColumns);
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes('fe_users', 'tx_rest_apikey;;;;1-1-1', '', 'after:password');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns(
+    'fe_users',
+    $tempColumns
+);
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes(
+    'fe_users',
+    'tx_rest_apikey;;;;1-1-1',
+    '',
+    'after:password'
+);
