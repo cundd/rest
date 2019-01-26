edff0c3a3070184b16f9499f502becc3c716b453
Daniel Corn <info@cundd.net>
2019-01-26 14:46:48 +0100

    Use `@method` instead of the Assert trait

--- Tests/Functional/AbstractCase.php
+++ Tests/Functional/AbstractCase.php
@@ -33,13 +33,26 @@ use Webmozart\Assert\Assert;
 
 /**
  * @method \Prophecy\Prophecy\ObjectProphecy prophesize($classOrInterface = null);
+ * @method void assertInternalType($expected, $actual, $message = '')
+ * @method void assertEquals($expected, $actual, $message = '', ...$args)
+ * @method void assertSame($expected, $actual, $message = '')
+ * @method void assertEmpty($actual, $message = '')
+ * @method void markTestSkipped($message = '')
+ * @method void markTestIncomplete($message = '')
+ * @method void assertInstanceOf($expected, $actual, $message = '')
+ * @method void assertArrayHasKey($key, $array, $message = '')
+ * @method void assertCount($expectedCount, $haystack, $message = '')
+ * @method void assertNotEquals($expected, $actual, $message = '', ...$args)
+ * @method void assertNull($actual, $message = '')
+ * @method void assertFalse($condition, $message = '')
+ * @method void assertTrue($condition, $message = '')
+ * @method void assertNotEmpty($actual, $message = '')
  */
 class AbstractCase extends FunctionalTestCase
 {
     use ResponseBuilderTrait;
     use RequestBuilderTrait;
     use ClassBuilderTrait;
-    use AssertTrait;
 
     /**
      * @var ObjectManager
