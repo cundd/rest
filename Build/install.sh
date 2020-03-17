08ab0c405ecbcc447ceed7794744f431d3b8b378
Daniel Corn <info@cundd.net>
2020-03-17 09:45:39 +0100

    Merge testing configuration from v4

--- Build/install.sh
+++ Build/install.sh
@@ -3,7 +3,7 @@
 set -o nounset
 set -o errexit
 
-PROJECT_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )";
+PROJECT_HOME="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 # The branch of TYPO3 to checkout (e.g. 'TYPO3_8-7', 'TYPO3_7-6')
 : ${TYPO3="master"}
@@ -20,53 +20,53 @@ PROJECT_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )";
 # @internal
 : ${TRAVIS_PHP_VERSION="7.0"}
 
-source "$PROJECT_HOME/Build/lib.sh";
-
+if [[ -e "lib.sh" ]]; then source "lib.sh"; fi
+source "$PROJECT_HOME/Build/lib.sh"
 
 # Install the project's dependencies
-function install_dependencies {
-    print_header "Install dependencies";
-    lib::composer self-update;
-    lib::composer install --verbose --ignore-platform-reqs;
+function install_dependencies() {
+    lib::print_header "Install dependencies"
+    lib::composer self-update
+    lib::composer install --verbose --ignore-platform-reqs
 }
 
 # Install the TYPO3
-function install_typo3 {
-    print_header "Get TYPO3 source $TYPO3";
+function install_typo3() {
+    lib::print_header "Get TYPO3 source $TYPO3"
 
-    local typo3_base_path=$(get_typo3_base_path);
+    local typo3_base_path=$(get_typo3_base_path)
     if [[ "$typo3_base_path" != "" ]]; then
-        lib::pushd "$typo3_base_path";
-        print_info "Update TYPO3 source";
-        git pull;
+        lib::pushd "$typo3_base_path"
+        lib::print_info "Update TYPO3 source"
+        git pull
     else
-        lib::pushd ..;
-        print_info "Install TYPO3 source $TYPO3";
+        lib::pushd ..
+        lib::print_info "Install TYPO3 source $TYPO3"
         if [[ ! -e "TYPO3.CMS" ]]; then
-            git clone --single-branch --branch "$TYPO3" --depth 1 git://git.typo3.org/Packages/TYPO3.CMS.git;
-            cd TYPO3.CMS;
+            git clone --single-branch --branch "$TYPO3" --depth 1 git://git.typo3.org/Packages/TYPO3.CMS.git
+            cd TYPO3.CMS
         fi
     fi
 
-    export TYPO3_PATH_WEB="`pwd`";
+    export TYPO3_PATH_WEB="$(pwd)"
 
-    if [ "$TRAVIS_PHP_VERSION" == "hhvm" ]; then
-        lib::composer remove --ignore-platform-reqs --dev friendsofphp/php-cs-fixer;
+    if [[ "$TRAVIS_PHP_VERSION" == "hhvm" ]]; then
+        lib::composer remove --ignore-platform-reqs --dev friendsofphp/php-cs-fixer
     fi
-    lib::composer install --ignore-platform-reqs --prefer-dist;
+    lib::composer install --ignore-platform-reqs --prefer-dist
 
-    rm -rf typo3/sysext/compatibility6;
+    rm -rf typo3/sysext/compatibility6
 
-    mkdir -p ./typo3conf/ext/;
+    mkdir -p ./typo3conf/ext/
     if [[ ! -e "./typo3conf/ext/$REPO" ]]; then
-        ln -s ${PROJECT_HOME} "./typo3conf/ext/$REPO";
+        ln -s ${PROJECT_HOME} "./typo3conf/ext/$REPO"
     fi
 
-    lib::popd;
+    lib::popd
 }
 
 # Prepares the MySQL database
-function prepare_database {
+function prepare_database() {
     if [[ "$(get_mysql_client_path)" != "" ]]; then
         if [[ "$typo3DatabasePassword" != "" ]]; then
             $(get_mysql_client_path) \
@@ -74,39 +74,39 @@ function prepare_database {
                 -u${typo3DatabaseUsername} \
                 -p${typo3DatabasePassword} \
                 -e "CREATE DATABASE IF NOT EXISTS $typo3DatabaseName;" || {
-                print_warning "Database $typo3DatabaseName not created";
-            };
+                lib::print_warning "Database $typo3DatabaseName not created"
+            }
         else
             $(get_mysql_client_path) \
                 -h${typo3DatabaseHost} \
                 -u${typo3DatabaseUsername} \
                 -e "CREATE DATABASE IF NOT EXISTS $typo3DatabaseName;" || {
-                print_warning "Database $typo3DatabaseName not created";
-            };
+                lib::print_warning "Database $typo3DatabaseName not created"
+            }
         fi
     else
-        print_warning "MySQL client not found";
+        lib::print_warning "MySQL client not found"
     fi
 }
 
 # Main entry point
-function main {
-    cd ${PROJECT_HOME};
+function main() {
+    cd ${PROJECT_HOME}
 
-    if [ "$#" -eq "0" ];then
-        install_dependencies;
-        install_typo3;
-        prepare_database;
-        return;
+    if [[ "$#" -eq "0" ]]; then
+        install_dependencies
+        install_typo3
+        prepare_database
+        return
     fi
 
-    local sub_command="$1";
-    shift;
+    local sub_command="$1"
+    shift
     if hash "$sub_command" 2>/dev/null; then
-        ${sub_command} "$@";
+        ${sub_command} "$@"
     else
-        print_error "Subcommand '$sub_command' does not exist";
+        lib::print_error "Subcommand '$sub_command' does not exist"
     fi
 }
 
-main "$@";
+main "$@"
--- Build/lib.sh
+++ Build/lib.sh
@@ -1,88 +1,88 @@
 #!/usr/bin/env bash
 
-PROJECT_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )";
+PROJECT_HOME="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 : ${DEBUG=""}
 
-function lib::_tput {
+lib::_tput() {
     : ${TERM="dumb"}
-    if [ "$TERM" != "dumb" ] && hash tput &>/dev/null; then
-        tput $*;
+    if [[ "$TERM" != "dumb" ]] && hash tput &>/dev/null; then
+        tput $*
     fi
 }
 
 # Print a error message
-function print_error() {
-    >&2 lib::_tput setaf 1;
-    >&2 echo "[ERROR] $@";
-    >&2 lib::_tput sgr0;
+lib::print_error() {
+    lib::_tput >&2 setaf 1
+    echo >&2 "[ERROR] $@"
+    lib::_tput >&2 sgr0
 }
 
 # Print a header message
-function print_header() {
-    lib::_tput setaf 2;
-    echo "[TASK] $@";
-    lib::_tput sgr0;
+lib::print_header() {
+    lib::_tput setaf 2
+    echo "[TASK] $@"
+    lib::_tput sgr0
 }
 
 # Print a info message
-function print_info() {
-    lib::_tput setaf 4;
-    echo "[INFO] $@";
-    lib::_tput sgr0;
+lib::print_info() {
+    lib::_tput setaf 4
+    echo "[INFO] $@"
+    lib::_tput sgr0
 }
 
 # Print a debug message
-function print_debug() {
-    if [ "$DEBUG" == "yes" ] || [ "$DEBUG" == "true" ]; then
-        >&2 echo "[DEBUG] $@";
+lib::print_debug() {
+    if [[ "$DEBUG" == "yes" ]] || [[ "$DEBUG" == "true" ]]; then
+        echo >&2 "[DEBUG] $@"
     fi
 }
 
 # Print a warning
-function print_warning() {
-    lib::_tput setaf 3;
-    echo "[WARNING] $@";
-    lib::_tput sgr0;
+lib::print_warning() {
+    lib::_tput setaf 3
+    echo "[WARNING] $@"
+    lib::_tput sgr0
 }
 
 # Retrieve the path to the MySQL client
-function get_mysql_client_path {
+get_mysql_client_path() {
     if hash mysql 2>/dev/null; then
-        echo mysql;
-    elif [[ `which mysql > /dev/null` ]]; then
-        which mysql;
+        echo mysql
+    elif [[ $(which mysql >/dev/null) ]]; then
+        which mysql
     elif [[ -x /Applications/MAMP/Library/bin/mysql ]]; then
-        echo /Applications/MAMP/Library/bin/mysql;
+        echo /Applications/MAMP/Library/bin/mysql
     else
-        return 1;
+        return 1
     fi
 }
 
 # Silent pushd
-function lib::pushd {
-    pushd $1 > /dev/null;
+lib::pushd() {
+    pushd $1 >/dev/null
 }
 
 # Silent popd
-function lib::popd {
-    popd > /dev/null;
+lib::popd() {
+    popd >/dev/null
 }
 
 # Detects the path to the TYPO3 base
-function get_typo3_base_path {
-    if [ -d "$PROJECT_HOME/../TYPO3.CMS" ]; then
-        echo "$PROJECT_HOME/../TYPO3.CMS";
-    elif [ -d "$PROJECT_HOME/../../../typo3" ]; then
-        dirname "$PROJECT_HOME/../../../typo3";
+get_typo3_base_path() {
+    if [[ -d "$PROJECT_HOME/../TYPO3.CMS" ]]; then
+        echo "$PROJECT_HOME/../TYPO3.CMS"
+    elif [[ -d "$PROJECT_HOME/../../../typo3" ]]; then
+        dirname "$PROJECT_HOME/../../../typo3"
     fi
 }
 
 # Run a composer command
-function lib::composer {
+lib::composer() {
     if hash composer.phar 2>/dev/null; then
-        composer.phar "$@";
+        composer.phar "$@"
     elif hash composer 2>/dev/null; then
-        composer "$@";
+        composer "$@"
     fi
 }
