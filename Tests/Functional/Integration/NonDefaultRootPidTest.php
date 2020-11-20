38a34d8840a2e760776bfcd3b473dcf64aa5d79f
Daniel Corn <info@cundd.net>
2020-11-20 19:39:11 +0100

    Add test for Root PID other than 1

--- Tests/Functional/Integration/NonDefaultRootPidTest.php
+++ Tests/Functional/Integration/NonDefaultRootPidTest.php
@@ -3,27 +3,25 @@ declare(strict_types=1);
 
 namespace Cundd\Rest\Tests\Functional\Integration;
 
-class GreetingTest extends AbstractGreetingCase
+class NonDefaultRootPidTest extends AbstractGreetingCase
 {
     /**
      * @dataProvider dataProviderTestLanguage
      * @param string $prefix
      * @param string $expectedMessage
      */
-    public function testLanguage(string $prefix, string $expectedMessage)
+    public function testNonDefaultRootPid(string $prefix, string $expectedMessage)
     {
         $this->importDataSet('ntf://Database/sys_language.xml');
-        $this->importDataSet('ntf://Database/tt_content.xml');
-        $this->importPages();
+        $this->importPagesWithRootId10();
 
-        // Setup the page with uid 1 and include the TypoScript as sys_template record
+        // Setup the page with uid 10 and include the TypoScript as sys_template record
         $this->setUpFrontendRootPage(
-            1,
+            10,
             [
                 'ntf://TypoScript/JsonRenderer.ts',
             ]
         );
-
         $this->fetchPathAndTestMessage($prefix, $expectedMessage);
     }
 }
