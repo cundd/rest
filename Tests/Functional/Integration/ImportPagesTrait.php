79ce51ac0671c96356b34454f88b02986196a513
Daniel Corn <info@cundd.net>
2020-11-21 13:06:40 +0100

    Fix test to not use pages_language_overlay on TYPO3 v9

--- Tests/Functional/Integration/ImportPagesTrait.php
+++ Tests/Functional/Integration/ImportPagesTrait.php
@@ -10,7 +10,7 @@ trait ImportPagesTrait
 {
     public function importPages()
     {
-        if (class_exists(Typo3Version::class) && (new Typo3Version())->getMajorVersion() >= 10) {
+        if (class_exists(Typo3Version::class) && (new Typo3Version())->getMajorVersion() >= 9) {
             $this->importDataSet(__DIR__ . '/../Fixtures/pages-modern-typo3.xml');
         } else {
             $this->importDataSet('ntf://Database/pages.xml');
@@ -20,7 +20,7 @@ trait ImportPagesTrait
 
     public function importPagesWithRootId10()
     {
-        if (class_exists(Typo3Version::class) && (new Typo3Version())->getMajorVersion() >= 10) {
+        if (class_exists(Typo3Version::class) && (new Typo3Version())->getMajorVersion() >= 9) {
             $this->importDataSet(__DIR__ . '/../Fixtures/pages-root-not-1-modern-typo3.xml');
         } else {
             $this->importDataSet(__DIR__ . '/../Fixtures/pages-root-not-1.xml');
