15cbed428413fdee80ee1c9362963c951360b357
Daniel Corn <daniel@iresults.li>
2021-07-13 12:36:43 +0200

    Enhance the `custom_rest` example extension

--- Tests/Functional/Fixtures/Extensions/custom_rest/ext_emconf.php
+++ Tests/Functional/Fixtures/Extensions/custom_rest/ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'version'          => '1.0.0',
     'constraints'      => [
         'depends'   => [
-            'typo3' => '6.0.0-9.5.99',
+            'typo3' => '9.5.0-10.99',
         ],
         'conflicts' => [
         ],
