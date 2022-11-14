9285af702b87c83a5d4878a801ab90de1309dfdf
Daniel Corn <daniel@iresults.li>
2022-11-14 14:44:27 +0100

    Change TYPO3 version constraints to allow TYPO3 v11

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = [
     'version'          => '5.1.0-dev',
     'constraints'      => [
         'depends'   => [
-            'typo3' => '9.5.0-10.4.99',
+            'typo3' => '9.5.0-11.5.99',
         ],
         'conflicts' => [],
         'suggests'  => [],
