be5b1c43b85620ad258a4afbf149a35b38e42388
Dominique <dominique.cinter@abteilung.ch>
2023-04-06 14:21:47 +0200

    Breaking: #78384 - Static file inclusion has moved from ext_tables.php to TCA/Overrides/sys_template.php. Extension Compatibility check will not pass.

--- ext_tables.php
+++ ext_tables.php
@@ -5,37 +5,10 @@ if (!defined('TYPO3_MODE')) {
 
 call_user_func(
     function () {
-        if (version_compare(TYPO3_version, '10.4.0') >= 0) {
-            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-                'rest',
-                'Configuration/TypoScript/Page/TYPO3-10',
-                'Virtual Object - Page'
-            );
-        } elseif (version_compare(TYPO3_version, '9.5.0') >= 0) {
-            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-                'rest',
-                'Configuration/TypoScript/Page/TYPO3-9',
-                'Virtual Object - Page'
-            );
-        } else {
-            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-                'rest',
-                'Configuration/TypoScript/Page/TYPO3-8',
-                'Virtual Object - Page'
-            );
-        }
-
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-            'rest',
-            'Configuration/TypoScript/Content',
-            'Virtual Object - Content'
-        );
-
-
         $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['reports']['rest']['handler'] = [
             'title'       => 'LLL:EXT:rest/Resources/Private/Language/locallang_db.xlf:reports.handler.title',
             'description' => 'LLL:EXT:rest/Resources/Private/Language/locallang_db.xlf:reports.handler.description',
-//            'icon' => 'EXT:rest/Resources/Public/Icons/tx_additionalreports_' . $report[1] . '.png',
+            // 'icon' => 'EXT:rest/Resources/Public/Icons/tx_additionalreports_' . $report[1] . '.png',
             'report'      => \Cundd\Rest\Documentation\HandlerReport::class,
         ];
     }
