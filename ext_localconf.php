83610ac2745223bcde1d11494abd8eda754b6806
Daniel Corn <daniel@iresults.li>
2026-04-22 16:44:44 +0200

    Breaking change: Drop support for TypoScript based configuration

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,5 @@
 <?php
 
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-
 defined('TYPO3') or die();
 
 (static function (): void {
@@ -10,6 +8,4 @@ defined('TYPO3') or die();
         || !is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cundd_rest_cache'])) {
         $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['cundd_rest_cache'] = [];
     }
-
-    ExtensionManagementUtility::addTypoScriptSetup("@import 'EXT:rest/Configuration/TypoScript/setup.typoscript'");
 })();
